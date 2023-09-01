3ebf8851d6be5d6568fe69b64c19098149630893
Miradil <miradild404@gmail.com>
2023-09-01 17:08:25 +0900

    empty

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,6 @@ fun main(args: Array<String>) {
     // Write a Python program to print the following string in a specific format (see the output).
     //Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
 
-    println("Twinkle, twinkle, little star,")
-    println("	How I wonder what you are!")
-    println("		Up above the world so high,")
-    println("		Like a diamond in the sky.")
-    println("Twinkle, twinkle, little star,")
-    println("	How I wonder what you are")
+
 
 }
